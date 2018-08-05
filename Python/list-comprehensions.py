c31bde066e86572ae6a8ddf46b8e3a26b6477af4
Anant Kaushik <anant.kaushik2@gmail.com>
2018-08-05 22:01:26 +0530

    List Comprehensions

---
+++ Python/list-comprehensions.py
@@ -0,0 +1,23 @@
+"""
+Let's learn about list comprehensions! You are given three integers X, Y and Z representing the dimensions of a cuboid along 
+with an integer N. You have to print a list of all possible coordinates given by (i,j,k) on a 3D grid where the sum 
+of i + j + k is not equal to N. Here 0 <= i <= X, 0 <= j <= Y, 0 <= k <= Z.
+
+Input Format
+Four integers X,Y,Z and K each on four separate lines, respectively.
+
+Constraints
+Print the list in lexicographic increasing order.
+
+Sample Input:
+1
+1
+1
+2
+
+Sample Output:
+[[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
+"""
+if __name__ == '__main__':
+    x,y,z,n = (int(input()) for _ in range(4))
+    print([[a,b,c] for a in range(x+1) for b in range(y+1) for c in range(z+1) if a+b+c != n])
