e5b1c6f4003654745c78d566ae35aaebb32c4f6b
anantkaushik <anant.kaushik2@gmail.com>
2019-03-01 23:51:07 +0530

    Insert a node at a specific position in a linked list

--- Interview-Preparation-Kit/Linked-Lists/insert-a-node-at-a-specific-position-in-a-linked-list.py
+++ Interview-Preparation-Kit/Linked-Lists/insert-a-node-at-a-specific-position-in-a-linked-list.py
@@ -1,5 +1,5 @@
 """
-Problem Link:  https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=linked-lists
+Problem Link:  https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list/problem
 
 You’re given the pointer to the head node of a linked list, an integer to add to the list and the position at 
 which the integer must be inserted. Create a new node with the given integer, insert this node at the desired 
