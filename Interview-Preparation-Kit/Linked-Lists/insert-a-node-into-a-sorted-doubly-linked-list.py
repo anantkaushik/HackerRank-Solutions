e0a3de855072e4d89c5cdd5b49d023cf83abfae3
anantkaushik <anant.kaushik2@gmail.com>
2019-03-01 23:52:46 +0530

    Inserting a Node Into a Sorted Doubly Linked List

--- Interview-Preparation-Kit/Linked-Lists/insert-a-node-into-a-sorted-doubly-linked-list.py
+++ Interview-Preparation-Kit/Linked-Lists/insert-a-node-into-a-sorted-doubly-linked-list.py
@@ -1,5 +1,5 @@
 """
-Problem Link: https://www.hackerrank.com/challenges/insert-a-node-into-a-sorted-doubly-linked-list/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=linked-lists
+Problem Link: https://www.hackerrank.com/challenges/insert-a-node-into-a-sorted-doubly-linked-list/problem
 
 Given a reference to the head of a doubly-linked list and an integer, data, create a new DoublyLinkedListNode object 
 having data value data and insert it into a sorted linked list while maintaining the sort.
