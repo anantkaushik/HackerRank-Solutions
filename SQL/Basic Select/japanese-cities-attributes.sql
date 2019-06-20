2e5bd9888ffd06926350cb298ec1aa71b8319cca
anantkaushik <anant.kaushik2@gmail.com>
2019-06-20 17:48:14 +0530

    Japanese Cities' Attributes

---
+++ SQL/Basic Select/japanese-cities-attributes.sql
@@ -0,0 +1,6 @@
+/*
+Problem Link: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+*/
+select * from CITY where COUNTRYCODE = "JPN";
