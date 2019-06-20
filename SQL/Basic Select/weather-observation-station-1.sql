3c6e7a9cb555430a9ee0fdf6b54859ac2c15e878
anantkaushik <anant.kaushik2@gmail.com>
2019-06-20 19:37:48 +0530

    Weather Observation Station 1

---
+++ SQL/Basic Select/weather-observation-station-1.sql
@@ -0,0 +1,6 @@
+/*
+Problem Link: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
+Query a list of CITY and STATE from the STATION table.
+*/
+select CITY, STATE from STATION;
