fded5b51e302b13782598e846bd0f8b3fb2d5258
Jivthesh M <jivtheshm@gmail.com>
2020-01-26 12:25:47 +0530

    Update string-validators.py

--- Python/Strings/string-validators.py
+++ Python/Strings/string-validators.py
@@ -25,6 +25,6 @@ True
 True
 """
 if __name__ == '__main__':
-    s = input()
+    s = raw_input()
     for method in [str.isalnum, str.isalpha, str.isdigit, str.islower, str.isupper]:
         print(any(method(i) for i in s))
