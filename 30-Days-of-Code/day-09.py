f281a241340ffa27bfb0154b067a04c3e6d3f16a
Siri Lalitha <sirilalitha9088@gmail.com>
2021-05-31 23:07:32 +0530

    Update day-09.py

--- 30-Days-of-Code/day-09.py
+++ 30-Days-of-Code/day-09.py
@@ -17,23 +17,34 @@ Sample Input
 
 Sample Output
 6
-"""
 #!/bin/python3
+
 import math
 import os
 import random
 import re
 import sys
 
-# Complete the factorial function below.
-def factorial(n):
-    if n == 1:
+#
+# Complete the 'factorial' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+def factorial(N):
+    '''Function to calculate the factorial'''
+    if N <= 1:
         return 1
-    return n * factorial(n-1)
-
+    else:
+        return N * factorial(N-1)
+    
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    n = int(input())
+
+    n = int(input().strip())
+
     result = factorial(n)
+
     fptr.write(str(result) + '\n')
+
     fptr.close()
